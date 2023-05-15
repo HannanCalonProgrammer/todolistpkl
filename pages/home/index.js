ea2b46fdb228ac8935756c1b5d40664732dd93de
HannanCalonProgrammer <hannankoes@gmail.com>
2023-05-15 09:46:05 +0700

    y

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,11 @@
-import Header from "../../components/Header"
-import Navbar from "../../components/navbar"
+import Header from "../../components/Header";
+import Navbar from "../../components/Navbar";
 
 export default function home() {
-  return(
+  return (
     <>
-    <Navbar />
-    <Header/>
+      <Navbar />
+      <Header />
     </>
-  )
+  );
 }
